b7951d958513ab9cea2ca51eac3ebf7cc947e202
Luis <ua.luis01@gmail.com>
2024-08-11 08:31:49 +0300

    Proxy

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM kalilinux/kali-rolling
 
-RUN apt update && apt install masscan python3 python3-pip git nano -y
+RUN apt update && apt install masscan python3 python3-pip git nano tor proxychains -y
 
 RUN git clone https://github.com/UaLuis/RTSPScanner.git
 
--- start.sh
+++ start.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+service tor start
+sleep 1
+
+service tor status
 nano target.txt
 
 sleep 1 
+proxychains ./rtspscan.sh
 
-./rtspscan.sh
+sleep 10
+proxychains python3 Bruter1.py -t ./masmap.txt -u ./user.txt -password admin run
+
+sleep 10
+proxychains python3 Bruter2.py
+
+
